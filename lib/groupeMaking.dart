1b97b40110476fc5020b6c0843a196d2ea4e2224
YlenNads <39728515+YlenNads@users.noreply.github.com>
2018-07-01 15:51:37 +0200

    Add files via upload

--- lib/groupeMaking.dart
+++ lib/groupeMaking.dart
@@ -12,50 +12,21 @@ class GroupMaking extends StatefulWidget{
   _GroupMaking createState() => new _GroupMaking(gruppen: gruppen);
 }
 
+
+String anzahl = '3';
 class _GroupMaking extends State<GroupMaking>
 {
-
-  bool begAnzahl;
-
   final List<Gruppen> gruppen;
   final pwKey = GlobalKey(debugLabel: 'ow');
   final formKey = GlobalKey<FormState>(debugLabel: 'inputText');
   String gruppenName = '2';
-  var anzahl;
   String password;
   _GroupMaking({Key key, @required this.gruppen});
-
 
   var pw = false;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-
-    /*Card pwCard = new Card( child: TextFormField(
-      // keyboardType: TextInputType.numberWithOptions(decimal: true, signed: false),
-      keyboardType: TextInputType.number,
-      style: TextStyle(color: Colors.white,fontSize: 20.0),
-      decoration: InputDecoration(
-          labelText: 'Passwort',
-          errorStyle: TextStyle(color: Colors.black, fontSize: 16.0),
-          labelStyle: TextStyle(color: Colors.white),
-          filled: true,
-          enabled: false,
-          fillColor: Colors.black38,
-          prefixIcon: Icon(Icons.lock, color: Colors.white,)
-      ),
-      // validator: (val) => val.contains('@') ? 'nicht erlaubt': null,
-      validator: (val){
-        if(val.contains(',') || val.contains('.') || val.contains(' ')){
-          return 'Nicht erlaubt!';
-        }else{
-          return null;
-        }
-      },
-      onSaved: (val) => anzahl = val,
-    ),);
-    */
     final Size screenSize = MediaQuery.of(context).size;
     return new Scaffold(
         appBar: new AppBar(
@@ -66,118 +37,42 @@ class _GroupMaking extends State<GroupMaking>
         body: ListView(
             children: <Widget>[
               Container(
-                padding: const EdgeInsets.all(20.0),
+                padding: const EdgeInsets.all(10.0),
                 child: Form(
                   key: formKey,
-                  child: Column(
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
+
                       Divider(height: 10.0,color: Colors.transparent,),
+
                       Card(
                         child: TextFormField(
                           keyboardType: TextInputType.text,
                           style: TextStyle(color: Colors.white,fontSize: 20.0),
                           decoration: InputDecoration(
-                            //icon: Icon(Icons.border_color, color: Colors.black,),
                             labelText: 'Gruppen Name',
                             errorStyle: TextStyle(color: Colors.black, fontSize: 16.0),
                             labelStyle: TextStyle(color: Colors.white),
-                            //hintStyle: TextStyle(color: Colors.black),
-                           // hintText: 'Gruppen Name',
                             filled: true,
                             fillColor: Colors.red.shade600,
                             prefixIcon: Icon(Icons.border_color, color: Colors.white,),
-                            // labelStyle: TextStyle(color: Colors.black,fontSize: 20.0)
                           ),
                           validator: (val) => val.length <1 ? 'Zu kurz' :null,
                           onSaved: (val) => gruppenName = val,
                         ),
                       ),
 
-                      Divider(height: 20.0, color: Colors.transparent,),
-                      Card(
-                        child: TextFormField(
-                         // keyboardType: TextInputType.numberWithOptions(decimal: true, signed: false),
-                          keyboardType: TextInputType.number,
-                          style: TextStyle(color: Colors.white,fontSize: 20.0),
-                          decoration: InputDecoration(
-                              labelText: 'Anzahl Mitglieder',
-                              errorStyle: TextStyle(color: Colors.black, fontSize: 16.0),
-                              labelStyle: TextStyle(color: Colors.white),
-                              filled: true,
-                              fillColor: Colors.red.shade600,
-                              prefixIcon: Icon(Icons.border_color, color: Colors.white,)
-                          ),
-                          validator: (val){
-                            if(val.contains(',') || val.contains('.') || val.contains(' ')){
-                              return 'Nicht erlaubt!';
-                            }else{
-                              return null;
-                            }
-                          },
-                          onSaved: (val) => anzahl = val,
-                        ),
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Text('Begrenzte Mitglieder:',style: TextStyle(color: Colors.black), ),
-                          Switch(
-                            activeColor: Colors.black,
-                            activeTrackColor: Colors.red,
-                            inactiveTrackColor: Colors.black45,
-                            value: bool.fromEnvironment('Anzahl', defaultValue: false),
-                            onChanged: (value){
-                              begAnzahl = true;
-                            },
-                          ),
-                        ],
-                      ),
-                      Divider(height: 50.0, color: Colors.transparent,),
-                      Column(
-                        children: <Widget>[
-                          Card(
-                            child: TextFormField(
-                              // keyboardType: TextInputType.numberWithOptions(decimal: true, signed: false),
-                              keyboardType: TextInputType.text,
-                              style: TextStyle(color: Colors.white,fontSize: 20.0),
-                              decoration: InputDecoration(
-                                  labelText: 'Passwort',
-                                  enabled: pw,
-                                  errorStyle: TextStyle(color: Colors.black, fontSize: 16.0),
-                                  labelStyle: TextStyle(color: Colors.white),
-                                  filled: true,
-                                  fillColor: Colors.red.shade600,
-                                  prefixIcon: Icon(Icons.lock, color: Colors.white,)
-                              ),
-                              validator: (val){
-                                if(val.contains(',') || val.contains('.') || val.contains(' ')){
-                                  return 'Nicht erlaubt!';
-                                  }else{
-                                  return null;
-                                  }
-                                  },
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Text('Passwort:',style: TextStyle(color: Colors.black), ),
-                          Switch(
-                            activeColor: Colors.black,
-                            activeTrackColor: Colors.red,
-                            inactiveTrackColor: Colors.black45,
-                            value: bool.fromEnvironment('Pw', defaultValue: false),
-                            onChanged: (value){
-                              pw = value;
-                            },
-                          ),
-                        ],
-                      ),
-                      Divider(color: Colors.transparent,height: 20.0,),
+                      Divider(height: 30.0, color: Colors.transparent,),
+
+                      MemberField(),
+
+                      Divider(height: 30.0, color: Colors.transparent,),
+
+                      PasswordField(),
+
+                      Divider(color: Colors.transparent,height: 60.0,),
+
                       RaisedButton(
                         color: Colors.red[800],
                         splashColor: Colors.black,
@@ -191,12 +86,13 @@ class _GroupMaking extends State<GroupMaking>
                         ),
                         onPressed: (){
                           _submit();
-                          gruppen.add(new Gruppen(gruppenName, anzahl));
+                          gruppen.add(new Gruppen(gruppenName, anzahl.toString()));
                           if(formKey.currentState.validate()){
                             Navigator.push(context, new MaterialPageRoute(builder: (context) => new Einladung(gruppen: gruppen,)));
                           }
                         },
                       ),
+
                     ],
                   ),
                 ),
@@ -211,4 +107,131 @@ class _GroupMaking extends State<GroupMaking>
       form.save();
     }
   }
+}
+
+class PasswordField extends StatefulWidget{
+  PasswordField({Key key}) : super (key: key);
+  @override
+  _PasswordField createState() => _PasswordField();
+}
+
+class _PasswordField extends State<PasswordField>{
+  bool _pwAktive = false;
+
+  void _handleTap(value){
+    setState(() {
+      //_pwAktive != _pwAktive;
+      if(_pwAktive){
+        _pwAktive = false;
+      }else{
+        _pwAktive = true;
+      }
+    });
+  }
+
+  Widget build(BuildContext context){
+    return Column(
+      children: <Widget>[
+        Card(
+          child: TextFormField(
+            keyboardType: TextInputType.text,
+            style: TextStyle(color: Colors.white,fontSize: 20.0),
+            decoration: InputDecoration(
+                labelText: 'Passwort',
+                enabled: _pwAktive,
+                errorStyle: TextStyle(color: Colors.black, fontSize: 16.0),
+                labelStyle: TextStyle(color: Colors.white),
+                filled: true,
+                fillColor: _pwAktive ? Colors.red.shade600 : Colors.black38,
+                prefixIcon: Icon(Icons.lock, color: Colors.white,)
+            ),
+            validator: (val){
+              if(val.contains(',') || val.contains('.') || val.contains(' ')){
+                return 'Nicht erlaubt!';
+              }else{
+                return null;
+              }
+            },
+          ),
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Text('Passwort:',style: TextStyle(color: Colors.black), ),
+            Checkbox(
+              activeColor: Colors.black,
+              value: _pwAktive ? true : false,
+              onChanged: _handleTap,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+}
+
+class MemberField extends StatefulWidget{
+  MemberField({Key key}) : super (key: key);
+  @override
+  _MemberField createState() => _MemberField();
+}
+
+class _MemberField extends State<MemberField>{
+  bool _mAktive = false;
+
+  void _handleTap(value){
+    setState(() {
+      if(_mAktive){
+        _mAktive = false;
+      }else{
+        _mAktive = true;
+      }
+    });
+  }
+
+  Widget build(BuildContext context){
+    return Column(
+      children: <Widget>[
+        Card(
+          child: TextFormField(
+            // keyboardType: TextInputType.numberWithOptions(decimal: true, signed: false),
+            keyboardType: TextInputType.number,
+            style: TextStyle(color: Colors.white,fontSize: 20.0),
+            decoration: InputDecoration(
+                labelText: 'Begrenzte Mitglieder:',
+                enabled: _mAktive,
+                errorStyle: TextStyle(color: Colors.black, fontSize: 16.0),
+                labelStyle: TextStyle(color: Colors.white),
+                filled: true,
+                fillColor: _mAktive ? Colors.red.shade600 : Colors.black38,
+                prefixIcon: Icon(Icons.border_color, color: Colors.white,)
+            ),
+            validator: (val){
+              if(val.contains(',') || val.contains('.') || val.contains(' ')){
+                return 'Nicht erlaubt!';
+              }else{
+                return null;
+              }
+            },
+            onSaved: (val) => anzahl = val,
+          ),
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Text('Passwort:',style: TextStyle(color: Colors.black), ),
+            Checkbox(
+              activeColor: Colors.black,
+              value: _mAktive ? true : false,
+              onChanged: _handleTap,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
 }
